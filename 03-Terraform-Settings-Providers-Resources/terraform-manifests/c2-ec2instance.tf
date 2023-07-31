894db49a782950da7df19521a614cf917968b261
golxadm23 <golxadm23@gmail.com>
2023-07-31 05:45:08 +0530

    cnages for ec2instance

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,5 +1,5 @@
 # Resource: EC2 Instance
-resource "aws_instance" "Test_VM" {
+resource "aws_instance" "myec2vm" {
   ami           = "ami-05548f9cecf47b442"
   instance_type = "t2.micro"
   user_data     = file("${path.module}/app1-install.sh")

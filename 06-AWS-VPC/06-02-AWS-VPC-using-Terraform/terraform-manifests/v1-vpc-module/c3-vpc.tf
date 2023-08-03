f951be1d5ca1008acbf5d890dfaed50732f3fb9a
golxadm23 <golxadm23@gmail.com>
2023-08-03 18:20:17 +0530

    changes

--- 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v1-vpc-module/c3-vpc.tf
+++ 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v1-vpc-module/c3-vpc.tf
@@ -1,21 +1,21 @@
 # Create VPC Terraform Module
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
   #version = "3.11.0"  
   # version = "~> 5.1"
-  version = "5.1.1"  
+  version = "5.1.1"
 
   # VPC Basic Details
-  name = "vpc-dev"
-  cidr = "10.0.0.0/16"   
-  azs                 = ["us-east-1a", "us-east-1b"]
-  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24"]
+  name            = "vpc-dev"
+  cidr            = "10.0.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   # Database Subnets
-  create_database_subnet_group = true
-  create_database_subnet_route_table= true
-  database_subnets    = ["10.0.151.0/24", "10.0.152.0/24"]
+  create_database_subnet_group       = true
+  create_database_subnet_route_table = true
+  database_subnets                   = ["10.0.151.0/24", "10.0.152.0/24"]
 
   #create_database_nat_gateway_route = true
   #create_database_internet_gateway_route = true
@@ -26,7 +26,7 @@ module "vpc" {
 
   # VPC DNS Parameters
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   public_subnet_tags = {
     Type = "public-subnets"
@@ -41,7 +41,7 @@ module "vpc" {
   }
 
   tags = {
-    Owner = "Mia"
+    Owner       = "Mia"
     Environment = "dev"
   }
 
